b624af92d110a3b1294e180f1692215610b9d281
Aldo Martinez <aldo.martinez@u-payments.com>
2022-07-12 12:25:31 -0400

    add arreglos

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,9 +1,13 @@
 import { Badge } from "@material-ui/core";
 import { ShoppingCartOutlined } from "@material-ui/icons";
+import { useContext } from "react";
+import { CartContext } from "./CartContext";
 
 const CartWidget = () => {
+    const test = useContext(CartContext);
+    console.log(test.cartList.length);
     return (
-        <Badge badgeContent={4} color="secondary">
+        <Badge badgeContent={test.cartList.length} color="secondary">
             <ShoppingCartOutlined />
         </Badge>
     );
